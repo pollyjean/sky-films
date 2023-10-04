4cf122ef3d61bf1673c30c995ab364ff354acf5e
sky(skysurfr) <skysurfr@me.com>
2023-10-04 23:53:47 +0900

    :alien: API 불러오기 확인

--- src/routers/Popular.tsx
+++ src/routers/Popular.tsx
@@ -1,5 +1,24 @@
+import { APIResponse, getPopular } from "@/api";
+import { useQuery } from "react-query";
+import { ErrorPage, Loading } from ".";
+
 const Popular = () => {
-  return <div>Popular</div>;
+  const { isLoading, isError, data } = useQuery<APIResponse>({ queryKey: ["popular"], queryFn: getPopular });
+  const page = data?.page;
+  const movieList = data?.results;
+  if (isError) return <ErrorPage />;
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <>
+      <ul>
+        {movieList?.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+      <div>{page}</div>
+    </>
+  );
 };
 
 export default Popular;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,3 +2,4 @@ export { default as Popular } from "./Popular";
 export { default as ComingSoon } from "./ComingSoon";
 export { default as ErrorPage } from "./ErrorPage";
 export { default as NowPlaying } from "./NowPlaying";
+export { default as Loading } from "./Loading";
