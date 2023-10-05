35326bed8c5ca2057b311cd10216b783d3eab7b6
sky(skysurfr) <skysurfr@me.com>
2023-10-05 21:30:03 +0900

    :art: 메뉴 만들고 링크 생성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1>Sky Films</h1>
+      <ul>
+        <li>
+          <Link to="/">Popular</Link>
+        </li>
+        <li>
+          <Link to="/coming-soon">Coming Soon</Link>
+        </li>
+        <li>
+          <Link to="/now-playing">Now Playing</Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
